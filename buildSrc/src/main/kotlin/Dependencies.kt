b80ae8460b1baa11ceaa9839aba90a1610b63692
Adrien Poupard <adrien@smartb.city>
2022-01-10 19:22:36 +0100

    Use last fixers version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,18 +1,17 @@
 import city.smartb.gradle.dependencies.FixersPluginVersions
 
 object PluginVersions {
-    const val fixers = "0.1.0"
-//    const val d2 = fixers
+    const val fixers = "0.3.1"
+    const val d2 = fixers
     const val kotlin = FixersPluginVersions.kotlin
 }
 
 object Versions {
-
     const val guava = "31.0.1-jre"
-    const val jacksonVersion = "2.9.8"
-    const val bouncycastleVersion = "1.65"
+    const val jacksonVersion = "2.13.0"
+    const val bouncycastleVersion = "1.70"
 
-    const val joseJwtVersion = "8.17"
+    const val joseJwtVersion = "9.15.2"
     const val jsonldJavaVersion = "1.1-SNAPSHOT"
 }
 
--- iris-ld/iris-ld-jsonld/build.gradle.kts
+++ iris-ld/iris-ld-jsonld/build.gradle.kts
@@ -5,9 +5,7 @@ plugins {
 
 dependencies {
 	implementation("com.github.jsonld-java:jsonld-java:${Versions.jsonldJavaVersion}")
-//	{
-//		changing = true
-//	}
+
 	api("com.fasterxml.jackson.core:jackson-core:${Versions.jacksonVersion}")
 	api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:${Versions.jacksonVersion}")
 	api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jacksonVersion}")
