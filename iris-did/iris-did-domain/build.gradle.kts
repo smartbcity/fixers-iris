c6f8876459e6410142c39610b69dbb5f25dd9f96
Teddy Le <teddy@smartb.city>
2023-02-01 14:50:36 +0100

    fix s2 version in did domain

--- iris-did/iris-did-domain/build.gradle.kts
+++ iris-did/iris-did-domain/build.gradle.kts
@@ -7,6 +7,6 @@ plugins {
 
 dependencies {
 	api("city.smartb.f2:f2-client-ktor:${Versions.f2}")
-	api("city.smartb.s2:s2-automate-dsl:0.10.0")
+	api("city.smartb.s2:s2-automate-dsl:${Versions.s2}")
 	api(project(":iris-ld:iris-did"))
 }
